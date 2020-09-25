cea38692c0551e6dc99ebe4c3257afb542bab1f4
Santiago Rugnitz <santiagorugnitz@gmail.com>
2020-09-24 21:02:44 -0300

    Db and test for AdminRepository

--- DataAccess/Repository.cs
+++ DataAccess/Repository.cs
@@ -1,8 +1,49 @@
-﻿using System;
+﻿using DataAccessInterface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataAccess
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private TourismContext context;
+        private readonly DbSet<T> DbSet;
+
+
+        public Repository(TourismContext context)
+        {
+            this.DbSet = context.Set<T>();
+            this.context = context;
+        }
+
+        public bool Add(T entity)
+        {
+            DbSet.Add(entity);
+            return true;
+        }
+
+        public bool Delete(T entity)
+        {
+            DbSet.Remove(entity);
+            return true;
+        }
+
+        public bool Exists(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return DbSet.ToList();
+        }
     }
 }
--- DataAccess/TourismContext.cs
+++ DataAccess/TourismContext.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace DataAccess
 {
-    class TourismContext
+    public class TourismContext : DbContext
     {
+        public DbSet<Administrator> Administrators { get; set; }
+
+        public TourismContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrator>()
+                .HasKey(x => x.Email);
+        }
+
+
+
     }
 }
