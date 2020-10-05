64f5f38860157b1b9f7dc6909175e04751043a5a
Martin <martin.gut@hotmail.com>
2020-10-05 19:32:52 -0300

    Implemented AdministratorRepository

--- DataAccess/AdministratorRepository.cs
+++ DataAccess/AdministratorRepository.cs
@@ -3,24 +3,50 @@ using Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess
 {
     public class AdministratorRepository : Repository<Administrator>, IAdministratorRepository
     {
+        private DbContext context;
+        private readonly DbSet<Administrator> DbSet;
+
         public AdministratorRepository(DbContext context) : base(context)
         {
+            this.DbSet = context.Set<Administrator>();
+            this.context = context;
         }
 
         public Administrator Find(string email, string password)
         {
-            throw new NotImplementedException();
+            var admins = DbSet.ToList().Where(admin => admin.Email == email
+            && admin.Password == password);
+
+            if (admins.Count() == 0)
+            {
+                return null;
+            }
+
+            return admins.ToArray()[0];
         }
 
         public bool IsLogged(string token)
         {
-            throw new NotImplementedException();
+            if (token == null)
+            {
+                return false;
+            }
+            
+            var admins = DbSet.ToList().Where(admin => admin.Token == token);
+
+            if (admins.Count() == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
