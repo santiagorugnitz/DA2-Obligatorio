af4f5095dadee49b986ab067cff44b84e112e7ba
Martin <martin.gut@hotmail.com>
2020-10-07 19:36:17 -0300

    Skeleton Ready

---
+++ WebApi/Controllers/ReservationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogicInterface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("reservations")]
+    public class ReservationController : Controller
+    {
+        private readonly IReservationHandler handler;
+
+        public ReservationController(IReservationHandler handler)
+        {
+            this.handler = handler;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ReservationModel reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromHeader] int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult ChangeState([FromBody] int state, int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
