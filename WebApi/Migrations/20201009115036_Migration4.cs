1c43ccc3b89d4a3b0cddabc258b4fef3cebb6513
Santiago Rugnitz <santiagorugnitz@gmail.com>
2020-10-09 08:56:46 -0300

    migration

---
+++ WebApi/Migrations/20201009115036_Migration4.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class Migration4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactInformation",
+                table: "Reservations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StateDescription",
+                table: "Reservations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telephone",
+                table: "Reservations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactInformation",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "StateDescription",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Telephone",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Reservations");
+        }
+    }
+}
