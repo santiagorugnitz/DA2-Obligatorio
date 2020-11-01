c4178dda3659156192bcc23808fb1bd38114abb1
Santiago Rugnitz <santiagorugnitz@gmail.com>
2020-11-01 19:32:29 -0300

    xml

--- Codigo Fuente/XmlFormatter/XmlFormatter.cs
+++ Codigo Fuente/XmlFormatter/XmlFormatter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Xml.Linq;
 using DataImport;
+using Microsoft.VisualBasic.CompilerServices;
+
 namespace XmlFormatter
 {
     public class XmlFormatter: IFormatter
@@ -15,24 +17,62 @@ namespace XmlFormatter
         {
             var param = sourceParameters[0];
 
+            var ret = new List<AccomodationImport>();
 
+            XDocument doc = XDocument.Load(param.Value);
 
-            //XDocument doc = XDocument.Load(param.Value);
+            foreach (XElement el in doc.Root.Elements())
+            {
+                ret.Add(new AccomodationImport
+                {
+                    Name = el.Attribute("name").Value,
+                    Stars = Double.Parse(el.Attribute("stars").Value),
+                    Address = el.Attribute("address").Value,
+                    ImageNames = GetImages(el),
+                    Fee = Double.Parse(el.Attribute("fee").Value),
+                    Description = el.Attribute("description").Value,
+                    Available = Boolean.Parse(el.Attribute("available").Value),
+                    Telephone = el.Attribute("telephone").Value,
+                    ContactInformation = el.Attribute("contact_information").Value,
+                    TouristSpot = getSpot(el.Element("TouristSpotImport"))
+                }) ;
+            }
+            return ret;
 
-            //foreach (XElement el in doc.Root.Elements())
-            //{
-            //    Console.WriteLine("{0} {1}", el.Name, el.Attribute("id").Value);
-            //    Console.WriteLine("  Attributes:");
-            //    foreach (XAttribute attr in el.Attributes())
-            //        Console.WriteLine("    {0}", attr);
-            //    Console.WriteLine("  Elements:");
+        }
 
-            //    foreach (XElement element in el.Elements())
-            //        Console.WriteLine("    {0}: {1}", element.Name, element.Value);
-            //}
+        private TouristSpotImport getSpot(XElement spot)
+        {
+            return new TouristSpotImport
+            {
+                Name = spot.Attribute("name").Value,
+                Description = spot.Attribute("description").Value,
+                RegionId = int.Parse(spot.Attribute("region_id").Value),
+                Image = spot.Attribute("image").Value,
+                CategoryIds = GetCategories(spot),
+            };
+        }
 
+        private List<int> GetCategories(XElement spot)
+        {
+            var ret = new List<int>();
 
-            return new List<AccomodationImport>();
+            foreach (var cat in spot.Element("Categories").Elements())
+            {
+                ret.Add(int.Parse(cat.Attribute("id").Value));
+            }
+            return ret;
+        }
+
+        private List<string> GetImages(XElement accomodation)
+        {
+            var ret = new List<string>();
+
+            foreach (var image in accomodation.Element("Images").Elements())
+            {
+                ret.Add(image.Attribute("name").Value);
+            }
+            return ret;
 
         }
     }
