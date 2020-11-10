d451a26742c2049ad1211a8a7cb3670fba28e2f3
Martin <martin.gut@hotmail.com>
2020-11-10 15:12:50 -0300

    Search facilities

--- FrontEnd/src/components/accommodations-search/accommodations-search.component.ts
+++ FrontEnd/src/components/accommodations-search/accommodations-search.component.ts
@@ -15,8 +15,17 @@ export class AccommodationsSearchComponent implements OnInit {
   startingDate = new Date();
   finishingDate = new Date();
   spot: TouristSpot;
-  
-  constructor(private fb: FormBuilder, private currentRoute: ActivatedRoute, private spotService: SpotService) {}
+  adultQuantity: number
+  retiredQuantity: number
+  childrenQuantity: number
+  babyQuantity: number
+
+  constructor(private fb: FormBuilder, private currentRoute: ActivatedRoute, private spotService: SpotService) {
+    this.adultQuantity = 1
+    this.retiredQuantity = 0
+    this.childrenQuantity = 0
+    this.babyQuantity = 0
+  }
   
   ngOnInit(): void {
     let id = +this.currentRoute.snapshot.params['spotId'];
@@ -34,4 +43,46 @@ export class AccommodationsSearchComponent implements OnInit {
   onSubmit() {
     alert('Thanks!');
   }
+
+  addAdult(){
+    this.adultQuantity++;
+  }
+
+  addRetired(){
+    this.retiredQuantity++;
+  }
+
+  addChild(){
+    this.childrenQuantity++;
+  }
+
+  addBaby(){
+    this.babyQuantity++;
+  }
+
+  removeAdult(){
+    if ((this.adultQuantity > 1 || this.retiredQuantity >= 1) && this.adultQuantity >0){
+      this.adultQuantity--;
+    }
+  }
+
+  removeRetired(){
+    if ((this.adultQuantity >= 1 || this.retiredQuantity > 1) && this.retiredQuantity >0){
+      this.retiredQuantity--;
+    }
+  }
+
+  removeChild(){
+    if (this.childrenQuantity >0){
+      this.childrenQuantity--;
+    }
+  }
+
+  removeBaby(){
+    if (this.babyQuantity >0){
+      this.babyQuantity--;
+    }
+  }
+
+
 }
