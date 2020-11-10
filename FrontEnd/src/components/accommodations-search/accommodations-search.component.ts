617ec2bc7fe6d58aa19d71c7214a156858652cce
Martin <martin.gut@hotmail.com>
2020-11-09 22:19:39 -0300

    Binding dates finished

--- FrontEnd/src/components/accommodations-search/accommodations-search.component.ts
+++ FrontEnd/src/components/accommodations-search/accommodations-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { TouristSpot } from 'src/models/tourist-spot';
 
 @Component({
@@ -9,12 +10,20 @@ import { TouristSpot } from 'src/models/tourist-spot';
 })
 export class AccommodationsSearchComponent {
   
-  startingDate = new FormControl(new Date());
-  finishingDate = new FormControl(new Date());
+  startingDate = new Date();
+  finishingDate = new Date();
   @Input() spot: TouristSpot;
 
   constructor(private fb: FormBuilder) {}
 
+  changeStartingDate(event: MatDatepickerInputEvent<Date>){
+    this.startingDate = event.value
+  }
+
+  changeFinishingDate(event: MatDatepickerInputEvent<Date>){
+    this.finishingDate = event.value
+  }
+  
   onSubmit() {
     alert('Thanks!');
   }
