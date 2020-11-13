3698509da896d9f4008575446daf3b87fd6e92aa
Santiago Rugnitz <santiagorugnitz@gmail.com>
2020-11-13 19:25:22 -0300

    accommodation address

--- FrontEnd/src/services/accommodation.service.ts
+++ FrontEnd/src/services/accommodation.service.ts
@@ -16,13 +16,13 @@ export class AccommodationService {
     images.push('https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/20/31/2031907_v2.jpeg')
     images.push('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT4nY1kUBFbEje1IUEXPPLvyAEnO2CRBXXMqA&usqp=CAU')
 
-    accommodations.push({ Name: 'Hotel 1', Id: 1, Images: images, Adress: 'Hotel 1', Stars: 2, Description: '', Fee: 300, Total: 0 })
+    accommodations.push({ Name: 'Hotel 1', Id: 1, Images: images, Address: 'Calle 1234', Stars: 2, Description: 'xd', Fee: 300, Total: 0 })
 
     const stars = 2.5;
-    accommodations.push({ Name: 'Hotel 2', Id: 2, Images: images, Adress: 'Hotel 2', Stars: stars, Description: '', Fee: 100, Total: 0 })
+    accommodations.push({ Name: 'Hotel 2', Id: 2, Images: images, Address: 'Hotel 2', Stars: stars, Description: '', Fee: 100, Total: 0 })
 
     const stars2 = 3.5;
-    accommodations.push({ Name: 'Hotel 3', Id: 3, Images: images, Adress: 'Hotel 3', Stars: stars2, Description: '', Fee: 200, Total: 0 })
+    accommodations.push({ Name: 'Hotel 3', Id: 3, Images: images, Address: 'Hotel 3', Stars: stars2, Description: '', Fee: 200, Total: 0 })
     return accommodations;
   }
 
